7d1f1eff7ae82ccadb652d62b96f30b7290ba775
Deo silaen <deokeldisilaen@gmail.com>
2025-01-03 03:16:48 +0700

    addPlayerRotation

--- Galaxy Strike/Assets/Scripts/PlayerMovement.cs
+++ Galaxy Strike/Assets/Scripts/PlayerMovement.cs
@@ -6,11 +6,14 @@ public class NewMonoBehaviourScript : MonoBehaviour
     [SerializeField] float controlSpeed = 10f;
     [SerializeField] float xClampRange = 5f;
     [SerializeField] float yClampRange = 5f;
+
+    [SerializeField] float controlRollFactor = 20f;
     Vector2 movemnt;
 
     void Update()
     {
         ProcessTranslation();
+        ProcessRotation();
     }
 
     public void OnMove(InputValue value){
@@ -27,5 +30,11 @@ public class NewMonoBehaviourScript : MonoBehaviour
         float rawYPos = transform.localPosition.y + yOffset;
         float clampedYPos = Mathf.Clamp(rawYPos, -yClampRange, yClampRange);
         transform.localPosition = new Vector3(clampedXPos, clampedYPos, 0f);
+    }
+
+    void ProcessRotation(){
+        Quaternion targetRotation = Quaternion.Euler(0f, 0f, -controlRollFactor * movemnt.x); 
+    
+        transform.localRotation = targetRotation;
     }
 }
