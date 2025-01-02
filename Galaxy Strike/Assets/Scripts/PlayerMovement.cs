5450e068f55be435f1ca33f98d12575a267f9095
Deo silaen <deokeldisilaen@gmail.com>
2025-01-03 02:54:36 +0700

    addPlayerController

--- Galaxy Strike/Assets/Scripts/PlayerMovement.cs
+++ Galaxy Strike/Assets/Scripts/PlayerMovement.cs
@@ -3,17 +3,22 @@ using UnityEngine.InputSystem;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
-    void Start()
-    {
-        
-    }
+    [SerializeField] float controlSpeed = 10f;
+    Vector2 movemnt;
 
     void Update()
     {
-        
+        ProcessTranslation();
     }
 
     public void OnMove(InputValue value){
-        Debug.Log(value.Get<Vector2>());
+        movemnt = value.Get<Vector2>();
+    }
+
+    private void ProcessTranslation()
+    {
+        float xOffset = movemnt.x * controlSpeed * Time.deltaTime;
+        float yOffset = movemnt.y * controlSpeed * Time.deltaTime;
+        transform.localPosition = new Vector3(transform.localPosition.x + xOffset, transform.localPosition.y + yOffset, 0f);
     }
 }
