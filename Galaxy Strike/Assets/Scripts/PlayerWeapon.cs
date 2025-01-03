1fa0c9e7e08097d691bdcdf3ecfe181f212a5da6
Deo silaen <deokeldisilaen@gmail.com>
2025-01-03 17:53:42 +0700

    addCrossHair

--- Galaxy Strike/Assets/Scripts/PlayerWeapon.cs
+++ Galaxy Strike/Assets/Scripts/PlayerWeapon.cs
@@ -4,9 +4,14 @@ using UnityEngine.InputSystem;
 public class PlayerWeapon : MonoBehaviour
 {
     [SerializeField] GameObject[] lasers;
+    [SerializeField] RectTransform crosshair;
     bool isFiring = false;
+    private void Start() {
+        Cursor.visible = false;
+    }
     private void Update() {
         ProcessFiring();
+        MoveCrosshair();
     }
     public void OnFire(InputValue value){
         isFiring = value.isPressed;
@@ -17,5 +22,9 @@ public class PlayerWeapon : MonoBehaviour
            var emmissionModule = laser.GetComponent<ParticleSystem>().emission;
            emmissionModule.enabled = isFiring;
       } 
+    }
+
+    void MoveCrosshair(){
+        crosshair.position = Input.mousePosition;
     }
 }
