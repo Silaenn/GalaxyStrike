70ae8bb4e5282c7f24fac9e89998b4bbbc428a28
Deo silaen <deokeldisilaen@gmail.com>
2025-01-03 17:08:35 +0700

    addInputLaser

---
+++ Galaxy Strike/Assets/Scripts/PlayerWeapon.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerWeapon : MonoBehaviour
+{
+    [SerializeField] GameObject laser;
+    bool isFiring = false;
+    private void Update() {
+        ProcessFiring();
+    }
+    public void OnFire(InputValue value){
+        isFiring = value.isPressed;
+    }
+
+    void ProcessFiring(){
+       var emmissionModule = laser.GetComponent<ParticleSystem>().emission;
+       emmissionModule.enabled = isFiring;
+    }
+}
