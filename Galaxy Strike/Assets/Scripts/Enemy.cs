44e019de131400e8cdaad6626dbff32094946644
Deo silaen <deokeldisilaen@gmail.com>
2025-01-04 01:29:14 +0700

    addCollisionParticelDestroyEnemy

--- Galaxy Strike/Assets/Scripts/Enemy.cs
+++ Galaxy Strike/Assets/Scripts/Enemy.cs
@@ -2,7 +2,9 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] GameObject destoryedVFX;
     private void OnParticleCollision(GameObject other) {
+        Instantiate(destoryedVFX, transform.position, Quaternion.identity);
         Destroy(this.gameObject);
     }
 }
