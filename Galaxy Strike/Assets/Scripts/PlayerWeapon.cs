0430f1b96c0e752555646ad878c68402066a66c1
Deo silaen <deokeldisilaen@gmail.com>
2025-01-03 21:44:55 +0700

    addScreenWorldPoint

--- Galaxy Strike/Assets/Scripts/PlayerWeapon.cs
+++ Galaxy Strike/Assets/Scripts/PlayerWeapon.cs
@@ -5,6 +5,8 @@ public class PlayerWeapon : MonoBehaviour
 {
     [SerializeField] GameObject[] lasers;
     [SerializeField] RectTransform crosshair;
+    [SerializeField] Transform targetPoint;
+    [SerializeField] float targetDistance = 100f;
     bool isFiring = false;
     private void Start() {
         Cursor.visible = false;
@@ -12,6 +14,7 @@ public class PlayerWeapon : MonoBehaviour
     private void Update() {
         ProcessFiring();
         MoveCrosshair();
+        MoveTargetPoint();
     }
     public void OnFire(InputValue value){
         isFiring = value.isPressed;
@@ -26,5 +29,10 @@ public class PlayerWeapon : MonoBehaviour
 
     void MoveCrosshair(){
         crosshair.position = Input.mousePosition;
+    }
+
+    void MoveTargetPoint(){
+        Vector3 targetPointPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, targetDistance);
+        targetPoint.position = Camera.main.ScreenToWorldPoint(targetPointPosition);
     }
 }
