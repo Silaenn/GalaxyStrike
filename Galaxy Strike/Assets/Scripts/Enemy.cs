899d2685aa9bbe7e874eabb6a17ad68039d98578
Deo silaen <deokeldisilaen@gmail.com>
2025-01-05 01:17:13 +0700

    addTuneDanTweak

--- Galaxy Strike/Assets/Scripts/Enemy.cs
+++ Galaxy Strike/Assets/Scripts/Enemy.cs
@@ -3,8 +3,20 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject destoryedVFX;
-    private void OnParticleCollision(GameObject other) {
-        Instantiate(destoryedVFX, transform.position, Quaternion.identity);
-        Destroy(this.gameObject);
+    [SerializeField] int hitPoints = 3;
+    private void OnParticleCollision(GameObject other)
+    {
+        ProcessHit();
+    }
+
+    private void ProcessHit()
+    {
+        hitPoints--;
+
+        if (hitPoints <= 0)
+        {
+            Instantiate(destoryedVFX, transform.position, Quaternion.identity);
+            Destroy(this.gameObject);
+        }
     }
 }
