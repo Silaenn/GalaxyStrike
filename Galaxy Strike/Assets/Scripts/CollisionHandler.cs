b4e81e389371d51f3cd80ce46608bf371f7f5abb
Deo silaen <deokeldisilaen@gmail.com>
2025-01-06 16:42:14 +0700

    addReloadLevel

--- Galaxy Strike/Assets/Scripts/CollisionHandler.cs
+++ Galaxy Strike/Assets/Scripts/CollisionHandler.cs
@@ -3,8 +3,13 @@ using UnityEngine;
 public class CollisionHandler : MonoBehaviour
 {
     [SerializeField] GameObject destoryedVFX;
+    GameSceneManager gameSceneManager;
+    private void Start() {
+        gameSceneManager = FindAnyObjectByType<GameSceneManager>();
+    }
 
     private void OnTriggerEnter(Collider other) {
+        gameSceneManager.ReloadLevel();
         Instantiate(destoryedVFX, transform.position, Quaternion.identity);
         Destroy(gameObject);
     }
